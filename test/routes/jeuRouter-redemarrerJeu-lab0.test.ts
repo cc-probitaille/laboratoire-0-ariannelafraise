09c448ff774d51a1009fd2f859578b357a311f57
Arianne <github@shield.paintilya.dev>
2025-09-10 18:23:15 -0400

    étape 3 - tests

--- test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -1,9 +1,24 @@
 // Vous devez insérer les nouveaux tests ici
 import { assert } from 'console';
 import 'jest-extended';
+import supertest from 'supertest';
+import app from '../../src/app';
 
-describe('redemarrerJeu.test.ts', () => {
-  it("devrait implémenter test", async () => {
-    throw new Error("Ce test n'a pas été défini")
+const request = supertest(app);
+
+describe('GET /api/v1/jeu/redemarrerJeu', () => {
+  beforeAll(async () => {
+    await request.post('/api/v1/jeu/demarrerJeu').send("Arianne")
+    await request.post('/api/v1/jeu/demarrerJeu').send("André-Louis")
   });
+
+  it('scénario principal (succès)', async () => {
+    const response = await request.get('/api/v1/jeu/redemarrerJeu');
+    expect(response.status).toBe(200);
+  });
+
+  it('post-conditions (plus de joueur)', async () => {
+    const response = await request.get('/stats');
+    expect(response.body.joueurs.length).toBe(0)
+  })
 });
